19f1556eb660527f7bf6657f3ce869c5afa28854
dheera <dheera@dheera.net>
2025-02-20 23:48:55 +0000

    init

--- batch_regression/__init__.py
+++ batch_regression/__init__.py
@@ -1,1 +1,4 @@
 from .linear import BatchLinearRegression
+from .quadratic import BatchQuadraticRegression
+from .cubic import BatchCubicRegression
+from .nonlinear import BatchNonlinearRegression
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="batch_linear_regression",
+    version="0.1.0",
+    description="A PyTorch-based library for batch linear regression on GPUs.",
+    author="Dheera Venkatraman",
+    author_email="dheera@dheera.net",
+    packages=find_packages(),
+    install_requires=[
+        "torch",  # Ensure that users have PyTorch installed.
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
