8c0b48d3874a7ce75f63fccaca14fb96afd15237
dheera <dheera@dheera.net>
2025-03-01 17:28:34 +0000

    Johansen test

--- batch_regression/__init__.py
+++ batch_regression/__init__.py
@@ -5,3 +5,4 @@ from .nonlinear import BatchNonlinearRegression
 from .multilinear import BatchMultilinearRegression
 from .kpss import kpss_test
 from .adf import adf_test
+from .johansen import johansen_test
